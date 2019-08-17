6bc7e2883c0cbff324583ca185d6bb784d72bc64
Clayton <schneiderc2016@gmail.com>
2019-08-17 18:38:00 -0400

    Completed reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
-import MobileMenu from './modules/mobile-menu'
+import MobileMenu from './modules/mobile-menu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jQuery';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll($(".feature-item"), "85%");
+var revealOnScroll = new RevealOnScroll($(".testimonial"), "60%");
